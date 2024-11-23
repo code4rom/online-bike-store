12e147da3b2d11218a230bfa22b67391f260797d
code4rom <romvladi888@gmail.com>
2024-11-22 21:30:06 -0700

    implemented role admin/user, checkout - to confirmation (viewOrders- not implemented ; sort/search - not. Logout - problem unsolved

---
+++ online-bike-store/src/test/java/com/bikestore/online_bike_store/OnlineBikeStoreApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bikestore.online_bike_store;
+
+import net.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBikeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
